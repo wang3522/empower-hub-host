cc9850058c88a1efa33856ed419f4a0cc963ace8
aconnollyBrunswick <anthony.connolly@oneasg.com>
2025-05-20 15:23:57 -0400

    Move file path of n2kclient

--- simulator/docker-run-command.sh
+++ simulator/docker-run-command.sh
@@ -2,6 +2,6 @@ docker run \
     --env "PYTHONUNBUFFERED=1" \
     --name hub-host-simulator \
     -v ./src:/simulator \
-    -v /Users/aconnolly/repos/empower-hub-host/HubInterfaces/HubInterface/N2KClient:/N2KClient \
+    -v /Users/aconnolly/repos/empower-hub-host/N2KClient:/N2KClient \
     -v ./config:/etc/dbus-1/system.d \
     hub-host-simulator:latest
