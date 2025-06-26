a9f611e8f6650453f4fff5756d125a78a268fc32
krushit-navico <181752870+krushit-navico@users.noreply.github.com>
2025-06-26 22:07:06 +0000

    Refactor monitoring data structures: rename member variables for consistency and clarity

--- HubN2KService/src/modules/czone/monitoringdata.h
+++ HubN2KService/src/modules/czone/monitoringdata.h
@@ -255,195 +255,195 @@ public:
 
 class Tank {
 public:
-  uint32_t Instance;
-  ValueU32 LevelPercent;
-  ValueU32 Level;
-  ValueU32 Capacity;
-  ValueTankType TankType;
+  uint32_t m_instance;
+  ValueU32 m_levelPercent;
+  ValueU32 m_level;
+  ValueU32 m_capacity;
+  ValueTankType m_tankType;
 
   bool operator==(const Tank &other) const = default;
 };
 
 class Engine {
 public:
-  uint32_t Instance;
-  ValueU32 Speed; // SC <unit: RPM, gain: 1> --> DP <unit: RPM, gain: 1> i.e. value: 1000 means 1000RPM * 1 = 1000RPM
-  ValueF BoostPressure;  // SC <unit: kPa, gain: 0.01> --> DP <unit: kPa, gain: 0.01> i.e. value: 8836 means 8836 * 0.01
-                         // = 88.36kPa = 12.82PSI
-  ValueS32 Trim;         // Not implemented
-  ValueU32 OilPressure;  // SC <unit: kPa, gain: 0.01> --> DP <unit: kPa, gain: 0.01> i.e. value: 34172 means 34712 *
-                         // 0.01 = 341.72kPa = 49.56PSI
-  ValueF OilTemperature; // SC <unit:degrees C, gain: 1> --> DP <unit:degrees C, gain: 1> i.e. value:100 means 100
-                         // degress C = 212 degress F
-  ValueF Temperature;    // SC <unit:degrees C, gain: 1> --> DP <unit:degrees C, gain: 1> (Sea water temperature) i.e.
-                         // value:-50 means -50 degress C
-  ValueF AlternatorPotential; // SC <unit: volts, gain: 0.001> --> DP <unit: volts, gain: 0.001> i.e. value: 13461 means
-                              // 13461 * 0.001 = 13.461V
-  ValueF FuelRate;            // Not implemented
-  ValueU32 TotalEngineHours;  // SC <unit: minutes, gain: 1> --> DP <unit: minutes, gain: 1> i.e. value: 586828 means
-                              // 586828 minutes = 9780 hours
-  ValueF CoolantPressure;     // SC <unit: kPa, gain: 0.01> --> DP <unit: kPa, gain: 0.01> i.e. value: 8836 means 8836 *
-                              // 0.01 = 88.36kPa = 12.82PSI
-  ValueF CoolantTemperature;  // SC <unit:degrees C, gain: 1> --> DP <unit:degrees C, gain: 1> i.e. value:63 means 63
-                              // degress C
-  ValueF FuelPressure;        // Not implemented
-  ValueU32 DiscreteStatus1;   // Not implemented
-  ValueU32 DiscreteStatus2;   // Not implemented
-  ValueS32 PercentEngineLoad; // Not implemented
-  ValueS32 PercentEngineTorque; // Not implemented
-  ValueEngineState EngineState; // SC <unit:enum 0-5, gain: 1> --> DP <unit:enum eEngineState>
-  ValueU32 ActiveEnginesId;
+  uint32_t m_instance;
+  ValueU32 m_speed; // SC <unit: RPM, gain: 1> --> DP <unit: RPM, gain: 1> i.e. value: 1000 means 1000RPM * 1 = 1000RPM
+  ValueF m_boostPressure;  // SC <unit: kPa, gain: 0.01> --> DP <unit: kPa, gain: 0.01> i.e. value: 8836 means 8836 *
+                           // 0.01 = 88.36kPa = 12.82PSI
+  ValueS32 m_trim;         // Not implemented
+  ValueU32 m_oilPressure;  // SC <unit: kPa, gain: 0.01> --> DP <unit: kPa, gain: 0.01> i.e. value: 34172 means 34712 *
+                           // 0.01 = 341.72kPa = 49.56PSI
+  ValueF m_oilTemperature; // SC <unit:degrees C, gain: 1> --> DP <unit:degrees C, gain: 1> i.e. value:100 means 100
+                           // degress C = 212 degress F
+  ValueF m_temperature;    // SC <unit:degrees C, gain: 1> --> DP <unit:degrees C, gain: 1> (Sea water temperature) i.e.
+                           // value:-50 means -50 degress C
+  ValueF m_alternatorPotential; // SC <unit: volts, gain: 0.001> --> DP <unit: volts, gain: 0.001> i.e. value: 13461
+                                // means 13461 * 0.001 = 13.461V
+  ValueF m_fuelRate;            // Not implemented
+  ValueU32 m_totalEngineHours;  // SC <unit: minutes, gain: 1> --> DP <unit: minutes, gain: 1> i.e. value: 586828 means
+                                // 586828 minutes = 9780 hours
+  ValueF m_coolantPressure; // SC <unit: kPa, gain: 0.01> --> DP <unit: kPa, gain: 0.01> i.e. value: 8836 means 8836 *
+                            // 0.01 = 88.36kPa = 12.82PSI
+  ValueF m_coolantTemperature;  // SC <unit:degrees C, gain: 1> --> DP <unit:degrees C, gain: 1> i.e. value:63 means 63
+                                // degress C
+  ValueF m_fuelPressure;        // Not implemented
+  ValueU32 m_discreteStatus1;   // Not implemented
+  ValueU32 m_discreteStatus2;   // Not implemented
+  ValueS32 m_percentEngineLoad; // Not implemented
+  ValueS32 m_percentEngineTorque; // Not implemented
+  ValueEngineState m_engineState; // SC <unit:enum 0-5, gain: 1> --> DP <unit:enum eEngineState>
+  ValueU32 m_activeEnginesId;
 
   bool operator==(const Engine &other) const = default;
 };
 
 class ACLine {
 public:
-  uint32_t Instance;
-  MeteringDevice::eACLine Line;
-  ValueF Voltage;
-  ValueF Current;
-  ValueF Frequency;
-  ValueF Power;
+  uint32_t m_instance;
+  MeteringDevice::eACLine m_line;
+  ValueF m_voltage;
+  ValueF m_current;
+  ValueF m_frequency;
+  ValueF m_power;
 
   bool operator==(const ACLine &other) const = default;
 };
 
 class AC {
 public:
-  uint32_t Instance;
-  std::unordered_map<uint32_t, std::shared_ptr<ACLine>> AClines;
+  uint32_t m_instance;
+  std::unordered_map<uint32_t, std::shared_ptr<ACLine>> m_acLines;
 
   bool operator==(const AC &other) const;
 };
 
 class DC {
 public:
-  uint32_t Instance;
-  ValueF Voltage;
-  ValueF Current;
-  ValueF Temperature;
-  ValueS32 StateOfCharge;
-  ValueF CapacityRemaining;
-  ValueU32 TimeRemaining; // minutes, current <= 0.0A
-  ValueU32 TimeToCharge;  // minutes, current > 0.0A
-  ValueU32 TimeRemainingOrToCharge;
+  uint32_t m_instance;
+  ValueF m_voltage;
+  ValueF m_current;
+  ValueF m_temperature;
+  ValueS32 m_stateOfCharge;
+  ValueF m_capacityRemaining;
+  ValueU32 m_timeRemaining;
+  ValueU32 m_timeToCharge;
+  ValueU32 m_timeRemainingOrToCharge;
 
   bool operator==(const DC &other) const = default;
 };
 
 class Temperature {
 public:
-  uint32_t Instance;
-  ValueF Temperature;
+  uint32_t m_instance;
+  ValueF m_temperature;
 
   bool operator==(const Temperature &other) const = default;
 };
 
 class Pressure {
 public:
-  uint32_t Instance;
-  ValueF Pressure;
+  uint32_t m_instance;
+  ValueF m_pressure;
 
   bool operator==(const Pressure &other) const = default;
 };
 
 class HVAC {
 public:
-  uint32_t Instance = 1;
-  ValueHVACOperatingMode OperationMode;
-  ValueU32 FanMode;                 // tbc
-  ValueU32 FanSpeed;                // Values from 1 to 125 = 1% to 125%. Auto 126
-  ValueF EnvironmentSetTemperature; // Celcius
-  ValueF EnvironmentTemperature;    // Celcius
+  uint32_t m_instance = 1;
+  ValueHVACOperatingMode m_operationMode;
+  ValueU32 m_fanMode;                 // tbc
+  ValueU32 m_fanSpeed;                // Values from 1 to 125 = 1% to 125%. Auto 126
+  ValueF m_environmentSetTemperature; // Celcius
+  ValueF m_environmentTemperature;    // Celcius
 
   bool operator==(const HVAC &other) const = default;
 };
 
 class ZipdeeAwning {
 public:
-  uint32_t Instance;
-  ValueAwningState State;
+  uint32_t m_instance;
+  ValueAwningState m_state;
 
   bool operator==(const ZipdeeAwning &other) const = default;
 };
 
 class ThirdPartyGenerator {
 public:
-  uint32_t Instance;
-  ValueU32 OnTime; // seconds
-  ValueGeneratorState Status;
+  uint32_t m_instance;
+  ValueU32 m_onTime; // seconds
+  ValueGeneratorState m_status;
 
   bool operator==(const ThirdPartyGenerator &other) const = default;
 };
 
 class InverterCharger {
 public:
-  uint32_t Instance; // Note in InverterInstance << 8 | ChargerInstance
-  ValueU32 InverterInstance;
-  ValueU32 ChargerInstance;
-  ValueInverterChargerEnabled InverterEnable;
-  ValueInverterState InverterState;
-  ValueInverterChargerEnabled ChargerEnable;
-  ValueChargerState ChargerState;
+  uint32_t m_instance; // Note in InverterInstance << 8 | ChargerInstance
+  ValueU32 m_inverterInstance;
+  ValueU32 m_chargerInstance;
+  ValueInverterChargerEnabled m_inverterEnable;
+  ValueInverterState m_inverterState;
+  ValueInverterChargerEnabled m_chargerEnable;
+  ValueChargerState m_chargerState;
 
   bool operator==(const InverterCharger &other) const = default;
 };
 
 class TyrePressure {
 public:
-  uint32_t Instance;
-  ValueF Pressure;
-  ValueF Temperature;
-  ValueTyreStatus Status;
-  ValueTyreLimitStatus LimitStatus;
+  uint32_t m_instance;
+  ValueF m_pressure;
+  ValueF m_temperature;
+  ValueTyreStatus m_status;
+  ValueTyreLimitStatus m_limitStatus;
 
   bool operator==(const TyrePressure &other) const = default;
 };
 
 class AudioStereo {
 public:
-  uint32_t Instance;
-  ValueBool Power;
-  ValueBool Mute;
-  ValueAudioStatus AudioStatus;
-  ValueAudioSource SourceMode;
-  ValueU32 Volume;
+  uint32_t m_instance;
+  ValueBool m_power;
+  ValueBool m_mute;
+  ValueAudioStatus m_audioStatus;
+  ValueAudioSource m_sourceMode;
+  ValueU32 m_volume;
 
   bool operator==(const AudioStereo &other) const = default;
 };
 
 class ACMainContactor {
 public:
-  uint32_t SystemStateId;
-  ValueContactorOnState ACContactorSystemsState;
-  ValueBool ACContactorSourceAvailable;
-  ValueBool ReversePolarity;
-  ValueBool ACContactorAutoChangeOver;
-  ValueBool ManualOverride;
+  uint32_t m_systemStateId;
+  ValueContactorOnState m_acContactorSystemsState;
+  ValueBool m_acContactorSourceAvailable;
+  ValueBool m_reversePolarity;
+  ValueBool m_acContactorAutoChangeOver;
+  ValueBool m_manualOverride;
 
   bool operator==(const ACMainContactor &other) const = default;
 };
 
 class GNSS {
 public:
-  uint32_t Instance;
-  ValueF Latitude;          // Radians, +/- North/South
-  ValueF Longitude;         // Radians, +/- East/West
-  ValueF Cog;               // Radians, True
-  ValueF Sog;               // m/s
-  ValueF MagneticVariation; // Radians
-  std::string UTCDateTime;  // ISO 8601 - YYYY-MM-DDThh:mm:ss.s
-  ValueU32 TimeOffset;      // Offset minutes
-  ValueU32 SatellitesInFix;
-  ValueS32 BestOfFourSatellitesSNR; // dB
-  ValueGNSSMethod Method;
-  ValueGNSSFixType FixType;
-  ValueF Hdop;              // unitless, lower is better
-  ValueF Pdop;              // unitless, lower is better
-  ValueF Vdop;              // unitless, lower is better
-  ValueDouble LatitudeDeg;  // Degrees
-  ValueDouble LongitudeDeg; // Degrees
+  uint32_t m_instance;
+  ValueF m_latitude;          // Radians, +/- North/South
+  ValueF m_longitude;         // Radians, +/- East/West
+  ValueF m_cog;               // Radians, True
+  ValueF m_sog;               // m/s
+  ValueF m_magneticVariation; // Radians
+  std::string m_utcDateTime;  // ISO 8601 - YYYY-MM-DDThh:mm:ss.s
+  ValueU32 m_timeOffset;      // Offset minutes
+  ValueU32 m_satellitesInFix;
+  ValueS32 m_bestOfFourSatellitesSNR; // dB
+  ValueGNSSMethod m_method;
+  ValueGNSSFixType m_fixType;
+  ValueF m_hdop;              // unitless, lower is better
+  ValueF m_pdop;              // unitless, lower is better
+  ValueF m_vdop;              // unitless, lower is better
+  ValueDouble m_latitudeDeg;  // Degrees
+  ValueDouble m_longitudeDeg; // Degrees
 
   bool operator==(const GNSS &other) const = default;
 };
@@ -452,54 +452,54 @@ class MonitoringKeyValue {
 public:
   bool m_valid;
   float m_value;
-  bool LimitValid;
-  float Min;
-  float Max;
-  float WarnLow;
-  float WarnHigh;
+  bool m_limitValid;
+  float m_min;
+  float m_max;
+  float m_warnLow;
+  float m_warnHigh;
 
   bool operator==(const MonitoringKeyValue &other) const = default;
 };
 
 class BinaryLogicState {
 public:
-  uint32_t Dipswitch;
-  uint32_t Instance;
-  ValueU32 States;
+  uint32_t m_dipswitch;
+  uint32_t m_instance;
+  ValueU32 m_states;
 
   bool operator==(const BinaryLogicState &other) const = default;
 };
 
 class NetworkStatus {
 public:
-  std::string EthernetStatus;
-  bool EthernetInternetConnectivity;
-  std::string EthernetIp;
-  std::string EthernetId;
-  std::string WifiStatus;
-  bool WifiInternetConnectivity;
-  std::string WifiIp;
-  std::string WifiSsid;
-  std::string WifiSecurity;
-  std::string WifiType;
-  int32_t WifiChannel;
-  int32_t WifiSignalStrengthDbm;
-  std::string HotspotStatus;
-  std::string HotspotIp;
-  std::string HotspotSsid;
-  std::string HotspotPassword;
-  std::string HotspotSecurity;
-  std::string HotspotType;
-  int32_t HotspotChannel;
-  std::string CellularStatus;
-  bool CelllularInternetConnectivity;
-  std::string CellularIp;
-  std::string CellularOperator;
-  std::string CellularType;
-  int32_t CellularSignalStrengthDbm;
-  std::string CellularSimIccid;
-  std::string CellularSimEid;
-  std::string CellularSimImsi;
+  std::string m_ethernetStatus;
+  bool m_ethernetInternetConnectivity;
+  std::string m_ethernetIp;
+  std::string m_ethernetId;
+  std::string m_wifiStatus;
+  bool m_wifiInternetConnectivity;
+  std::string m_wifiIp;
+  std::string m_wifiSsid;
+  std::string m_wifiSecurity;
+  std::string m_wifiType;
+  int32_t m_wifiChannel;
+  int32_t m_wifiSignalStrengthDbm;
+  std::string m_hotspotStatus;
+  std::string m_hotspotIp;
+  std::string m_hotspotSsid;
+  std::string m_hotspotPassword;
+  std::string m_hotspotSecurity;
+  std::string m_hotspotType;
+  int32_t m_hotspotChannel;
+  std::string m_cellularStatus;
+  bool m_celllularInternetConnectivity;
+  std::string m_cellularIp;
+  std::string m_cellularOperator;
+  std::string m_cellularType;
+  int32_t m_cellularSignalStrengthDbm;
+  std::string m_cellularSimIccid;
+  std::string m_cellularSimEid;
+  std::string m_cellularSimImsi;
 
   bool operator==(const NetworkStatus &other) const;
   void clear();
@@ -510,43 +510,43 @@ public:
   SnapshotInstanceIdMap() = default;
   ~SnapshotInstanceIdMap() = default;
 
-  std::unordered_map<uint32_t, std::shared_ptr<Circuit>> m_Circuits;
-  std::unordered_map<uint32_t, std::shared_ptr<Circuit>> m_Modes;
-  std::unordered_map<uint32_t, std::shared_ptr<Tank>> m_Tanks;
-  std::unordered_map<uint32_t, std::shared_ptr<Engine>> m_Engines;
-  std::unordered_map<uint32_t, std::shared_ptr<AC>> m_AC;
-  std::unordered_map<uint32_t, std::shared_ptr<DC>> m_DC;
-  std::unordered_map<uint32_t, std::shared_ptr<Temperature>> m_Temperatures;
-  std::unordered_map<uint32_t, std::shared_ptr<Pressure>> m_Pressures;
-  std::unordered_map<uint32_t, std::shared_ptr<HVAC>> m_Hvacs;
-  std::unordered_map<uint32_t, std::shared_ptr<ZipdeeAwning>> m_Awnings;
-  std::unordered_map<uint32_t, std::shared_ptr<ThirdPartyGenerator>> m_ThirdPartyGenerators;
-  std::unordered_map<uint32_t, std::shared_ptr<InverterCharger>> m_InverterChargers;
-  std::unordered_map<uint32_t, std::shared_ptr<TyrePressure>> m_Tyrepressures;
-  std::unordered_map<uint32_t, std::shared_ptr<AudioStereo>> m_AudioStereos;
-  std::unordered_map<uint32_t, std::shared_ptr<ACMainContactor>> m_ACMainContactors;
-  std::unordered_map<uint32_t, std::shared_ptr<GNSS>> m_GNSS;
-  std::unordered_map<uint32_t, std::shared_ptr<MonitoringKeyValue>> m_MonitoringKeyValue;
-  std::unordered_map<uint32_t, std::shared_ptr<BinaryLogicState>> m_BinaryLogicState;
-  NetworkStatus m_NetworkStatus;
-  std::string m_TimeStamp;
+  std::unordered_map<uint32_t, std::shared_ptr<Circuit>> m_circuits;
+  std::unordered_map<uint32_t, std::shared_ptr<Circuit>> m_modes;
+  std::unordered_map<uint32_t, std::shared_ptr<Tank>> m_tanks;
+  std::unordered_map<uint32_t, std::shared_ptr<Engine>> m_engines;
+  std::unordered_map<uint32_t, std::shared_ptr<AC>> m_ac;
+  std::unordered_map<uint32_t, std::shared_ptr<DC>> m_dc;
+  std::unordered_map<uint32_t, std::shared_ptr<Temperature>> m_temperatures;
+  std::unordered_map<uint32_t, std::shared_ptr<Pressure>> m_pressures;
+  std::unordered_map<uint32_t, std::shared_ptr<HVAC>> m_hvacs;
+  std::unordered_map<uint32_t, std::shared_ptr<ZipdeeAwning>> m_awnings;
+  std::unordered_map<uint32_t, std::shared_ptr<ThirdPartyGenerator>> m_thirdPartyGenerators;
+  std::unordered_map<uint32_t, std::shared_ptr<InverterCharger>> m_inverterChargers;
+  std::unordered_map<uint32_t, std::shared_ptr<TyrePressure>> m_tyrepressures;
+  std::unordered_map<uint32_t, std::shared_ptr<AudioStereo>> m_audioStereos;
+  std::unordered_map<uint32_t, std::shared_ptr<ACMainContactor>> m_acMainContactors;
+  std::unordered_map<uint32_t, std::shared_ptr<GNSS>> m_gnss;
+  std::unordered_map<uint32_t, std::shared_ptr<MonitoringKeyValue>> m_monitoringKeyValue;
+  std::unordered_map<uint32_t, std::shared_ptr<BinaryLogicState>> m_binaryLogicState;
+  NetworkStatus m_networkStatus;
+  std::string m_timeStamp;
 
   void Clear();
 };
 
 class HealthStatus {
 public:
-  eHealth ServiceThread;
-  eHealth NetworkThread;
-  eHealth SCThread;
-  eHealth GNSSThread;
-  eHealth GNSSLatLon;
-  eHealth GNSSFix;
+  eHealth m_serviceThread;
+  eHealth m_networkThread;
+  eHealth m_scThread;
+  eHealth m_gnssThread;
+  eHealth m_gnssLatLon;
+  eHealth m_gnssFix;
 };
 
 class MonitoringKeyValueMap {
 public:
-  std::unordered_map<uint32_t, std::shared_ptr<MonitoringKeyValue>> KeyValueMap;
+  std::unordered_map<uint32_t, std::shared_ptr<MonitoringKeyValue>> m_keyValueMap;
 
   void Clear();
 };
